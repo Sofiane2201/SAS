b88badde38a70e28463aa449fae2bd0af1b5a9f7
sofiane <sofiane22@outlook.be>
2023-02-02 09:56:51 +0100

    SAS

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build/style.css">
+    <title>school web site</title>
+</head>
+<body>
+<div class="slide" id="home">
+    
+    <header>
+        <div id="info">
+            <div class="wrapper">
+                <div id="pratique">
+                    <span id="tel">Tel: 02/ 395.60.23</span>
+                    <span id="email">E-mail:info@epse.be</span>
+
+                </div>
+                <div id="social">Soial media iconv_mime_decode_headers</div>
+            </div>
+        </div>
+        
+        <div id="header">
+            <div class="wrapper">
+                <div id="logo">Site name</div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">home</a>
+                        </li>
+                        <li>
+                            <a href="#">schools</a>
+                        </li>
+                        <li>
+                            <a href="#">contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+</div>
+</body>
+</html>
